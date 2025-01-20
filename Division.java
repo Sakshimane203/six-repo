32080eeadfbc2930fcc63fb24dc1d50021bfdc4a
sakshi mane <manesakshi203@gmail.com>
2025-01-20 22:20:06 +0530

    Division .java with initial content

---
+++ Division.java
@@ -0,0 +1,15 @@
+class Division {
+public static void main(String[]args){
+int a=10,b=5,c=5,result;
+try{
+result=a/(b-c);
+System.out.println("result:"+result);
+}
+catch(ArithmeticException e){
+System.out.println("Exception Cought! Division by zero");
+}
+finally{
+System.out.println("I am in Final block");
+}
+}
+}
